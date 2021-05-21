96c7dca2f8982fc859f4d7ce935015b30eb35bce
Jasc94 <jonathansuarezcaceres@gmail.com>
2021-05-21 10:56:26 +0200

    Tarea de titanic terminada

---
+++ 0_Tools/Graphs_library.py
@@ -0,0 +1,10 @@
+## Función para crear varios gráficos a la vez
+
+def graphs():
+    #TODO
+
+    fig, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey = True)
+
+    sns.histplot(titanic["age"], bins = 10, ax = ax1)
+    sns.histplot(titanic["age"], bins = 20, ax = ax2)
+    sns.histplot(titanic["age"], bins = 50, ax = ax3)
